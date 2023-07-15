692fcf8ed2f60334e6cf61751cae319550de727f
emodeth <emodethistaken@gmail.com>
2023-07-15 20:53:38 +0300

    Responsive navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [isSticky, setIsSticky] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(function () {
     function handleSticky() {
@@ -16,21 +17,44 @@ export default function Navbar() {
   });
 
   return (
-    <nav className={isSticky ? "navbar sticky-navbar" : "navbar"}>
-      <div className="navbar-container">
-        <a href="/">COINDOM</a>
-        <ul>
-          <li>Home</li>
-          <li>Market</li>
-          <li>Choose Us</li>
-          <li>Join</li>
-        </ul>
+    <>
+      <nav className={isSticky ? "navbar sticky-navbar" : "navbar"}>
+        <div className="navbar-container">
+          <a href="/">COINDOM</a>
+          <ul>
+            <li>Home</li>
+            <li>Market</li>
+            <li>Choose Us</li>
+            <li>Join</li>
+          </ul>
 
-        <span className="icons">
-          <i className="fa-brands fa-twitter"></i>
-          <i className="fa-brands fa-discord"></i>
-        </span>
-      </div>
-    </nav>
+          <span className="icons">
+            <i className="fa-brands fa-twitter"></i>
+            <i className="fa-brands fa-discord"></i>
+          </span>
+        </div>
+      </nav>
+
+      <nav className="navbar__mobile">
+        <h1>COINDOM</h1>
+        <button onClick={() => setToggle(true)}>
+          <i class="fa-solid fa-bars-staggered hamburger-menu"></i>
+        </button>
+      </nav>
+
+      <ul
+        className={
+          toggle ? "navbar__mobile-menu menu-up" : "navbar__mobile-menu"
+        }
+      >
+        <button onClick={() => setToggle(false)}>
+          <i class="fa-solid fa-xmark close-mobile"></i>
+        </button>
+        <li onClick={() => setToggle(false)}>Home</li>
+        <li onClick={() => setToggle(false)}>Market</li>
+        <li onClick={() => setToggle(false)}>Choose Us</li>
+        <li onClick={() => setToggle(false)}>Join</li>
+      </ul>
+    </>
   );
 }
